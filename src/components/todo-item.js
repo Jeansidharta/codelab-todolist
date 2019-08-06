981beef3719cb4600921a7c2baf19849b8c36faf
jean sidharta <jeansidharta@gmail.com>
2019-08-05 21:30:17 -0700

    Ädded backend

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -3,11 +3,22 @@ import React from "react";
 export function TodoItem(props){
 	const text = props.text || "";
 	const onDeleteFunction = props.onDelete || (()=>{});
+	const [isDeleting, setIsDeleting] = React.useState(false);
+
+	React.useEffect(()=>{
+		if(!isDeleting) return;
+		onDeleteFunction();
+	}, [isDeleting]);
 
 	return (
 		<div className="todo-item">
 			<p>{text}</p>
-			<button onClick={onDeleteFunction}>×</button>
+			<button
+				onClick={()=>setIsDeleting(true)}
+				className={isDeleting? "disabled" : ""}
+			>
+				×
+			</button>
 		</div>
 	);
 }
--- src/pages/App.js
+++ src/pages/App.js
@@ -3,31 +3,102 @@ import React from 'react';
 import { TodoItem } from "../components/todo-item";
 import "../assets/styles/index.css";
 
+const todoURL = "https://us-central1-codelab-todo-2d49f.cloudfunctions.net/todo";
+// const todoURL = "http://localhost:5001/codelab-todo-2d49f/us-central1/todo";
+
+async function getAllTodos(){
+	let response;
+	try{
+		response = await fetch(todoURL);
+	} catch(e) {
+		alert("Connection failed");
+		return false;
+	}
+	if(response.status !== 200){
+		console.error(response);
+		alert("Error: Bad request");
+		return false;
+	}
+	let newTodos = (await response.json()).data;
+	return Object.entries(newTodos).map(([id, todo]) => {
+		todo.key = id;
+		return todo;
+	});
+}
+
+async function createTodo(newTodo){
+	let response;
+	try{
+		response = await fetch(todoURL, {
+			method: "POST",
+			body: JSON.stringify(newTodo),
+		});
+	} catch(e) {
+		alert("Connection failed");
+		return false;
+	}
+	if(response.status !== 200){
+		console.error(response);
+		alert("Error: Bad request");
+		return false;
+	}
+	newTodo.key = (await response.json()).data;
+	return newTodo;
+}
+
+async function deleteTodo(newTodoID){
+	let response;
+	try{
+		response = await fetch(`${todoURL}?id=${newTodoID}`, {
+			method: "DELETE"
+		});
+	} catch(e) {
+		alert("Connection failed");
+		return false;
+	}
+	if(response.status !== 200){
+		console.error(response);
+		alert("Error: Bad request");
+		return false;
+	}
+	return true;
+}
+
 export function App() {
 	const textareaRef = React.useRef();
 	const [todos, setTodos] = React.useState([]);
+	const submitButton = React.useRef();
+	const [isLoading, setIsLoading] = React.useState(true);
 
-	function submit(event){
+	React.useEffect(()=>{(async () => {
+		const newTodos = await getAllTodos();
+		if(newTodos)
+			setTodos([...newTodos]);
+		setIsLoading(false);
+	})();}, []);
+
+	async function submit(event){
+		setIsLoading(true);
 		event.preventDefault();
 		const value = textareaRef.current.value.trim();
 		if(!value){
 			textareaRef.current.classList.add("error");
 			alert("Cannot create an empty todo!");
 			return;
-		} else {
-			textareaRef.current.classList.remove("error");
 		}
-
-		const newTodo = {id: Date.now(), text: value};
-
+		textareaRef.current.classList.remove("error");
 		textareaRef.current.value = "";
-
-		setTodos([...todos, newTodo]);
+		const newTodo = await createTodo({text: value});
+		if(newTodo)
+			setTodos([...todos, newTodo]);
+		setIsLoading(false);
 	}
 
-	function deleteItem(item){
-		todos.splice(todos.indexOf(item), 1);
-		setTodos([...todos]);
+	async function deleteItem(item){
+		if(await deleteTodo(item.key)){
+			todos.splice(todos.indexOf(item), 1);
+			setTodos([...todos]);
+		}
 	}
 
 	return (
@@ -35,13 +106,19 @@ export function App() {
 			<h1>Todo List</h1>
 			<form onSubmit={submit}>
 				<textarea ref={textareaRef}/>
-				<button type="submit">add</button>
+				<button
+					ref={submitButton}
+					type="submit"
+					className={isLoading? "disabled" : "enabled"}
+				>
+					add
+				</button>
 			</form>
 			<ul>
 				{
 					todos.map(todo => (
 						<TodoItem
-							key={todo.id}
+							key={todo.key}
 							text={todo.text}
 							onDelete={()=>deleteItem(todo)}
 						/>
